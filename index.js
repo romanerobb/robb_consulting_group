fcc8f41f77e768ad2f4e9eda776803b0c40c7eb6
Romane Orlando Robb <mail@romanerobb.com>
2022-07-21 11:40:46 -0400

    Updated JS.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-let developer = "Romane Robb";
+let developer = "Romane Orlando Robb";
 console.log(developer);
+
+function robbDeveloperAlert() {
+    alert("This website was created by Romane Orlando Robb!");
+  }
+  
+  console.log(robbDeveloperAlert());
